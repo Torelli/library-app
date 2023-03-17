e66b953b7329ba565771a5ea73d279ac8dda9da1
Giovanni Torelli <giovanni.walter@outlook.com>
2023-03-17 00:45:25 -0300

    Added modal form for user input

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const themeButton = document.querySelector("#theme-btn");
+const themeTooltip = document.querySelector("#theme-tooltip");
+const btnCloseModal = document.querySelector("#btn-close-modal");
+const modal = document.querySelector("#modal");
+// const modalBackdrop = document.querySelector("#modal-backdrop");
+const btnNewBook = document.querySelector("#btn-new-book");
+
+function toggleModal() {
+  if (modal.getAttribute("aria-modal") == "true") {
+    modal.setAttribute("aria-modal", "false");
+  } else {
+    modal.setAttribute("aria-modal", "true");
+  }
+}
 
 function changeTheme() {
   if (
@@ -7,20 +20,28 @@ function changeTheme() {
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
     document.documentElement.classList.add("dark");
+    themeTooltip.textContent = "Turn off dark mode";
   } else {
     document.documentElement.classList.remove("dark");
+    themeTooltip.textContent = "Turn on dark mode";
   }
 }
 
 document.onload = changeTheme();
 
-themeButton.addEventListener('click', () => {
-    console.log(localStorage.theme);
-    if (document.documentElement.classList.contains("dark")) {
-        localStorage.theme = 'light';
-        changeTheme();
-    } else {
-        localStorage.theme = 'dark';
-        changeTheme();
-    }
+btnCloseModal.addEventListener("click", toggleModal);
+
+// modalBackdrop.addEventListener("click", toggleModal);
+
+btnNewBook.addEventListener("click", toggleModal);
+
+themeButton.addEventListener("click", () => {
+  console.log(localStorage.theme);
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "light";
+    changeTheme();
+  } else {
+    localStorage.theme = "dark";
+    changeTheme();
+  }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,39 @@ module.exports = {
   darkMode: 'class',
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        closeModal: {
+          '0%' : {
+            visibility: 'visible'
+          },
+          '100%' : {
+            visibility: 'hidden'
+          }
+        },
+        fade: {
+          '0%': {
+            transform: 'translateY(-1.4em)'
+          },
+          '100%': {
+            transform: 'translateY(0)'
+          }
+        },
+        help: {
+          '0%, 4%': {
+            opacity: 0
+          },
+          '5%, 100%': {
+            opacity: 1
+          }
+        }
+      },
+      animation: {
+        fade: 'fade 0.2s',
+        help: 'help 80s',
+        closeModal: 'closeModal 0s forwards 0.15s'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
